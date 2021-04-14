d4f3b916411ee327404bce55cd22e643e80e6445
astutimega <putrimega141@gmail.com>
2021-04-14 10:57:37 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin/home', [AdminController::class, 'index'])
+    ->name('admin.home')
+    ->middleware('is_admin');
